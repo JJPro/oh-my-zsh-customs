831a9eb1d4a49fd4995ebcca0a91cb6328659e49
LU JI <lu.ji1@me.com>
2018-12-17 04:56:05 -0500

    custom themes and LSCOLORS

---
+++ LSCOLORS.zsh
@@ -0,0 +1,4 @@
+# a: black, b: red, c: green, d: yellow, e: purple, f: magenta, g: cyan, h: white
+
+export LSCOLORS=ExGxcxdxCxegedabagacad # Unix
+# export LS_COLORS=di=1;34:ln=1;36:so=32:pi=33:ex=1;32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43 # Linux
